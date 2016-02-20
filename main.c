125d547539b671986be8e0f91ce7a2a5e56ebff3
AnsataMamoru <king.williamd.wdk@gmail.com>
2016-02-20 00:05:12 -0700

    Commit includes testing code written in main.c, and methods mem_init and mem_free.

--- main.c
+++ main.c
@@ -1,3 +1,5 @@
+/* Authors: William King & Ivo Georgiev */
+
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
@@ -66,6 +68,38 @@ int main(int argc, char *argv[]) {
     status = mem_free();
 
     printf("status = %d\n", status);
+    assert(status == ALLOC_OK);
+
+    /* Personal & Pre-change Testing Code */
+
+    printf("%ld\n", __STDC_VERSION__);
+    const unsigned POOL_SIZE = 1000000;
+    pool_pt pool = NULL;
+
+    /* Test mem_init */
+    alloc_status status = mem_init();
+    assert(status == ALLOC_OK);
+
+    /* Test mem_pool_open */
+    pool = mem_pool_open(POOL_SIZE, FIRST_FIT); // TODO add policy to pool
+    assert(pool);
+
+    /* Test mem_new_alloc */
+    alloc_pt alloc1 = mem_new_alloc(pool, POOL_SIZE);
+    assert(alloc1);
+    alloc_pt alloc2 = mem_new_alloc(pool, POOL_SIZE);
+    assert(!alloc2);
+
+    /* Test mem_delete_alloc */
+    status = mem_del_alloc(pool, alloc1);
+    assert(status == ALLOC_OK);
+
+    /* Test mem_pool_close */
+    status = mem_pool_close(pool);
+    assert(status == ALLOC_OK);
+
+    /* Test mem_free */
+    status = mem_free();
     assert(status == ALLOC_OK);
 
     return 0;
